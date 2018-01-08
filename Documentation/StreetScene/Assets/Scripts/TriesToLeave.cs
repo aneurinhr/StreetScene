94841a4fd8d193ba529733487eca0b9d7e88e0c6
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-01-08 12:44:22 +0000

    Added VoiceLines
    Voicelines added

---
+++ Documentation/StreetScene/Assets/Scripts/TriesToLeave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriesToLeave : MonoBehaviour {
+
+    public AudioClip triesToLeave;
+
+    private bool voiceLine = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (voiceLine == false)
+        {
+            GetComponent<AudioSource>().PlayOneShot(triesToLeave);
+            voiceLine = true;
+            StartCoroutine(ExecuteVoiceline());
+        }
+    }
+
+    IEnumerator ExecuteVoiceline()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        voiceLine = false;
+    }
+
+}
