711b26f4467d0804e64e9d161a2a379963ee3967
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-01-11 19:54:04 +0000

    Feedback
    Progress Bar
    Fixed boundary hitboxes.

--- Documentation/StreetScene/Assets/Scripts/ShopDoors.cs
+++ Documentation/StreetScene/Assets/Scripts/ShopDoors.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
 
 public class ShopDoors : MonoBehaviour {
 
@@ -11,8 +12,11 @@ public class ShopDoors : MonoBehaviour {
     public GameObject interactUI;
     public GameObject person;
 
+    public Slider progressBar;
+
     public string doorTag = "";
     public float lockPickTime;
+    public float currentProgress = 0.0f;
 
     public float rotateSpeed = 3.0f;
 
@@ -70,6 +74,12 @@ public class ShopDoors : MonoBehaviour {
             transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
         }
 
+        if (m_lockPicking == true)
+        {
+            currentProgress = currentProgress + Time.deltaTime;
+            progressBar.value = currentProgress;
+        }
+
     }
 
     IEnumerator Unlock()
@@ -78,11 +88,17 @@ public class ShopDoors : MonoBehaviour {
         person.GetComponent<FirstPersonController>().enabled = false;
         m_lockPicking = true;
 
+        progressBar.gameObject.SetActive(true);
+        currentProgress = 0.0f;
+
         yield return new WaitForSeconds(lockPickTime);
 
         person.GetComponent<FirstPersonController>().enabled = true;
         m_locked = false;
         m_lockPicking = false;
+
+        progressBar.gameObject.SetActive(false);
+        currentProgress = lockPickTime;
     }
 
     IEnumerator Open()
